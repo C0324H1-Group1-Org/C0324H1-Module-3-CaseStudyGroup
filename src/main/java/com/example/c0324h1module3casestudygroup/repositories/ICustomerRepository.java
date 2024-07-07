8757640ba5f4f81a376eef8b3dda3d5f88832613
dungnguyen <dungna.qn@gmail.com>
2024-07-07 09:42:39 +0700

    add cart function

--- src/main/java/com/example/c0324h1module3casestudygroup/repositories/ICustomerRepository.java
+++ src/main/java/com/example/c0324h1module3casestudygroup/repositories/ICustomerRepository.java
@@ -1,7 +1,10 @@
 package com.example.c0324h1module3casestudygroup.repositories;
 
+import com.example.c0324h1module3casestudygroup.dto.CartDTO;
 import com.example.c0324h1module3casestudygroup.models.Account;
 import com.example.c0324h1module3casestudygroup.models.Customer;
+
+import java.util.List;
 
 public interface ICustomerRepository {
     boolean isValidUser(java.lang.String username, java.lang.String password);
@@ -9,4 +12,11 @@ public interface ICustomerRepository {
     boolean isUsernameTaken(String username);
 
     void register(Customer customer, Account account);
+
+
+    List<CartDTO> getCart(String username);
+
+    void deleteCartItem(int idCart, int idProduct);
+
+    void updateCartItem(int idCartUpdate, int idProductUpdate, int inputQuantity);
 }
